b0b7555f5bc8978a0af15ffc0678a46a28524e1d
GreyBeard42 <greybeard4200@yahoo.com>
2024-10-02 00:40:33 +0000

    look function froggoten line

--- script.js
+++ script.js
@@ -172,6 +172,7 @@ class Prey {
                     temp.mood = e.mood
                     this.vision = temp
                 }
+                bestDist = thisDist
             }
         })
     }
